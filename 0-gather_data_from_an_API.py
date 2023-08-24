013bddf3390d5df34ed94271d37473dfb9257ec3
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-24 12:16:50 -0500

    api

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""it returns infomration of a to do list of an employee"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    to_do: requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
+                         sys.argv[1], timeout=5)
+    name = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                         sys.argv[1], timeout=5)
+    
+    json_todo = to_do.json()
+    json_names = name.jason()
+
+    all_tasks = 0
+    tasks_completed = 0
+
+    titles_completed=[]
+    for task in json_todo:
+        all_tasks += 1
+        if task["completed"] is True:
+            tasks_completed += 1
+            titles_completed.append(task["title"])
+
+    print('L\'employé {} a terminé ({}/{}) de ses tâches :'
+          .format(json_names['name'], tasks_completed, all_tasks))
+    
+    for title_task in titles_completed:
+        print('\t {}'.format(title_task))
