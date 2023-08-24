74e8120083c1171386efff4c63045d88e3ccdc1d
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-24 15:56:22 -0500

    API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""using REST API to identify a given employee"""
-
+""" Getting my first apis """
+import json
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    """using REST API Placeholder through parameter"""
-    parametro_id = sys.argv[1]
-
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={parametro_id}"
-    url_nombre = f"https://jsonplaceholder.typicode.com/users/{parametro_id}"
-
-    respuesta = requests.get(url)
-    respuesta_nombre = requests.get(url_nombre)
-
-    total_tarea = respuesta.json()
-    informacion_empleado = respuesta_nombre.json()
-    nombre_empleado = informacion_empleado.get("name")
-
-    tarea_completadas = []
-    for tarea in total_tarea:
-        if tarea["completed"]:
-            tarea_completadas.append(tarea)
-    cantidad_tarea_completada = len(tarea_completadas)
-    cantidad_total_tarea = len(total_tarea)
-
-    print(f"Employee {nombre_empleado} is done \
-with tasks({cantidad_tarea_completada}/{cantidad_total_tarea}):")
-
-    for tarea in tarea_completadas:
-        print(f"\t {tarea.get('title')}")
-        
+    """Get API"""
+    todos_api = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/')
+    user_api = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    todo_data = todos_api.text
+    user_data = user_api.text
+    user = json.loads(user_data)
+    todos = json.loads(todo_data)
+    completed = []
+    all_todos = 0
+    for todo in todos:
+        if todo['userId'] == user['id']:
+            if todo['completed']:
+                completed.append(todo)
+            all_todos += 1
+    print(
+        'Employee {} is done with tasks({}/{}):'
+        .format(user['name'], len(completed), all_todos), file=sys.stdout)
+    for finished_todo in completed:
+        print('\t {}'.format(finished_todo['title']), file=sys.stdout)
